ae0d8fe54fb22c1a217a652c07d19e62e85a632e
Dragos Frunza <dragosfrunza@gmail.com>
2020-06-30 16:39:59 +0100

    commented the code

--- a2.R
+++ a2.R
@@ -15,12 +15,12 @@ library(rgdal)
 library(dplyr)
 library(shinyWidgets)
 
+#reading in roan and elephant data
+eles<- read.csv(paste('../Disso/elemydatuse.csv', sep=''))
+roan <- read.csv(paste('../Disso/roanmydatuse.csv', sep=''))
 
-eles<- read.csv(paste('./elemydatuse.csv', sep=''))
-roan <- read.csv(paste('./roanmydatuse.csv', sep=''))
-
-
-covar_shape<-readOGR(dsn = paste('./Shapes/covariate shape files', sep=''), layer = 'covariates_grid_ref')
+#reading covariate data and transforming location param
+covar_shape<-readOGR(dsn = paste('../Disso/covariate shape files', sep=''), layer = 'covariates_grid_ref')
 covardat<-covar_shape@data
 covardat<-data.frame(covardat, coordinates(covar_shape))
 xy <- data.frame(ID = 1:nrow(covardat), X = covardat$coords.x1, Y = covardat$coords.x2)
@@ -31,7 +31,7 @@ covardat$x.pos<- res@coords[,1]
 covardat$y.pos<- res@coords[,2]
 covardat$area<-0.25
 
-
+#trasforming roan and elephant data to long-lat
 mapdata <- eles
 coordinates(mapdata) <- ~x.pos+y.pos #similar to SpatialPoints
 proj4string(mapdata) <- CRS("+proj=utm +zone=33 +south") #assign projection and coordinate reference system
@@ -46,13 +46,16 @@ longlats2 <- as.data.frame(longlats2)
 
 
 
-
+#ui
 ui <- fluidPage(
   
-  leafletOutput("mymap"),
+  #map containig the response
+  leafletOutput("response_map"),
   
-  leafletOutput("map2"),
+  #map-containing the covariates
+  leafletOutput("covariate_map"),
   
+  #slider
   sliderTextInput(inputId = "slider1",
                   label = "Select Month and Year" ,
                   choices = unique(longlats$yearmonth),
@@ -63,8 +66,11 @@ ui <- fluidPage(
                   post = NULL, dragRange = TRUE)
 )
 
+
+#server
 server <- function(input, output, session) {
   
+  #reactive data object for filtering from the slider input
   filteredData <- reactive({
     longlats %>%
       filter(yearmonth == input$slider1)
@@ -75,35 +81,45 @@ server <- function(input, output, session) {
       filter(yearmonth == input$slider1)
   })
   
-  output$mymap <- renderLeaflet({
+  #building the response map
+  output$response_map <- renderLeaflet({
     leaflet() %>% addTiles() %>%
+      
+      #heatmaps for the response variales
       addHeatmap(data = filteredData(), lng = ~x.pos, lat = ~y.pos, intensity = ~response, radius=5, gradient = 'Reds', blur = 10,
                  group = 'Response Ele') %>%
       addHeatmap(data = filteredData2(), lng = ~x.pos, lat = ~y.pos, intensity = ~response, radius=5, gradient = 'Blues', blur = 10,
                  group = 'Response Roan') %>%
+      
+      #checkbox for response variables
       addLayersControl(
         overlayGroups =c("Response Ele","Response Roan"),
         options = layersControlOptions(collapsed=FALSE)
       )
   })
   
-  
-  output$map2 <- renderLeaflet({
+  #building the covariate map
+  output$covariate_map <- renderLeaflet({
     leaflet() %>% addTiles() %>%
+      
+      #heatmaps for the covariates
       addHeatmap(data=longlats, lng = ~x.pos, lat = ~y.pos, intensity = ~mindistToWaters/1000, radius = 6, gradient = 'Greys',
                  group = 'water') %>%
       addHeatmap(data=longlats, lng = ~x.pos, lat = ~y.pos, intensity = ~mindistToFence/1000, radius = 6, gradient = 'Greens',
                  group = 'fence') %>%
       addHeatmap(data=covardat, lng = ~coords.x1, lat = ~coords.x2, intensity = ~altitude,radius = 5 ,gradient = 'PuRd',
                  group = 'altitude') %>%
+      #checkbox for covariates
       addLayersControl(
         overlayGroups =c("water","fence","altitude"),
         options = layersControlOptions(collapsed=FALSE)
       )
   })
 
+  
+    #the reactive part of the map: the response datapoints for elephants and covariates so we don't rerender the whole map everytime
     observe({
-      leafletProxy("mymap") %>%
+      leafletProxy("reponse_map") %>%
       clearHeatmap() %>%
       addHeatmap(data=filteredData(), lng = ~x.pos, lat = ~y.pos, intensity = ~response, radius=5, gradient = 'Reds', blur = 10,
               group = 'Response Ele') %>%
